a7e7f476af440a8835d58dce4fd9caae7ab73472
ulysses-ck <ulisesapaza.280@gmail.com>
2022-11-29 08:26:02 -0300

    Finish: base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'piano';
+	applySound(soundNumber: number) {
+		const audio = new Audio();
+		audio.src = '../assets/sounds/note' + soundNumber + '.wav';
+		audio.load();
+		audio.play();
+	}
 }
